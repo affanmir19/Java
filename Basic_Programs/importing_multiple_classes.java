d0299903d00acdcd54c5026f1f6e07cbb97a1520
MIR AFAN <84376469+affanmir19@users.noreply.github.com>
2024-10-03 10:48:52 +0530

    Create importing_multiple_classes.java

---
+++ Basic_Programs/importing_multiple_classes.java
@@ -0,0 +1,14 @@
+// Importing multiple classes from the java.util package
+import java.util.ArrayList;
+import java.util.List;
+
+public class MultipleClassImport {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.add("Apple");
+        list.add("Banana");
+        list.add("Cherry");
+        
+        System.out.println("Fruits: " + list);
+    }
+}
